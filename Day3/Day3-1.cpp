13107bc7a3befe3280944d6d827475ca32ff3d19
Franz <139059326+franz053@users.noreply.github.com>
2024-12-03 09:58:04 +0100

    Solution Day3-1

---
+++ Day3/Day3-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <fstream>
+
+#define INPUT_PATH "C:\\Users\\krempl\\Downloads\\input.txt"
+
+int main(){
+    std::ifstream InFile(INPUT_PATH);
+    std::string currentLine;
+    const std::regex expression("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
+    int result = 0;
+
+    while (std::getline(InFile, currentLine)) {
+        
+        std::smatch match;
+        while(std::regex_search(currentLine,match,expression)){
+            std::string found = match.str();
+            int num1 = std::stoi(found.substr(4,found.find(',')-1));
+            int num2 = std::stoi(found.substr(found.find(',')+1,found.length()-1));
+            result += num1*num2;
+            currentLine = match.suffix().str();
+        }
+    }
+    std::cout << result;
+}
