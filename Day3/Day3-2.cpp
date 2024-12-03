e2f4182dcb48263d9412569e4e8e1bbe6e80b4c7
Franz <139059326+franz053@users.noreply.github.com>
2024-12-03 10:26:18 +0100

    Solution Day3-2

---
+++ Day3/Day3-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <regex>
+#include <string>
+#include <fstream>
+
+#define INPUT_PATH "C:\\Users\\krempl\\Downloads\\Day3\\input.txt"
+
+int main(){
+    std::ifstream InFile(INPUT_PATH);
+    std::string currentLine;
+    const std::regex expression("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do(n't)?\\(\\)");
+    int result = 0;
+    bool enabled = true;
+    while (std::getline(InFile, currentLine)) {
+        
+        std::smatch match;
+        while(std::regex_search(currentLine,match,expression)){
+            std::string found = match.str();
+            if(found == "do()") enabled = true;
+            else if(found == "don't()") enabled = false;
+            else if(enabled){
+                int num1 = std::stoi(found.substr(4,found.find(',')-1));
+                int num2 = std::stoi(found.substr(found.find(',')+1,found.length()-1));
+                result += num1*num2;
+            }
+            currentLine = match.suffix().str();
+        }
+    }
+    std::cout << result;
+}
