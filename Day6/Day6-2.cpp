b25c39116b6a09373d4c0cfc5d2aa4d36cf3fce5
Franz <139059326+franz053@users.noreply.github.com>
2024-12-06 19:05:21 +0100

    made 6-2 more efficient by only checking locations on the original Path

--- Day6/Day6-2.cpp
+++ Day6/Day6-2.cpp
@@ -33,13 +33,35 @@ int main() {
     int height = static_cast<int>(labClear.size()) - 1;
     int width = static_cast<int>(labClear.at(0).size()) - 1;
 
+    //mark path of possibilities
+    pair<int, int> guard = guardStart;
+    int direction = 0b10;
+    while (guard.first < height && guard.first > 0 && guard.second < width && guard.second > 0) {
+        int nextY = guard.first + ((!(direction & 0b01)) * (0b01 - (direction & 0b10))); //Determine movement
+        int nextX = guard.second + ((direction & 0b01) * (0b01 - (direction & 0b10))); //Determine movement
+        if(labClear[nextY][nextX] > 3){
+            //loop, visited same field at least 3 times
+            ++result;
+            break;
+        }else if (labClear[nextY][nextX] == -1) {
+            //obstacle
+            direction = (direction + 3) % 4;
+        } else {
+            //no obstacle
+            guard.first = nextY;
+            guard.second = nextX;
+            labClear[nextY][nextX] = 1;
+        }
+    }
+
+    //check all possibilities
     for (int boxRow = 0; boxRow <= height; ++boxRow) {
         for (int boxColumn = 0; boxColumn <= width; ++boxColumn) {
-            if(labClear[boxRow][boxColumn] == -1) continue;
+            if(labClear[boxRow][boxColumn] != 1) continue;
             //RESET
-            pair<int, int> guard = guardStart;
+            guard = guardStart;
             vector<vector<char>> lab = labClear;
-            int direction = 0b10;
+            direction = 0b10;
             // 1 0
             // | | 0 x-axis, 1 y-axis
             // | 0 positive, 1 negative
@@ -51,7 +73,7 @@ int main() {
                 int nextY = guard.first + ((!(direction & 0b01)) * (0b01 - (direction & 0b10))); //Determine movement
                 int nextX = guard.second + ((direction & 0b01) * (0b01 - (direction & 0b10))); //Determine movement
                 if(lab[nextY][nextX] > 3){
-                    //loop, visited same field at least 3 times
+                    //loop, visited same field more than 3 times
                     ++result;
                     break;
                 }else if (lab[nextY][nextX] == -1) {
