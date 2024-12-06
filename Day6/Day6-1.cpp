995b8a0c05e69915d0e6ce5c72782fbdf9f2c0d6
Franz <139059326+franz053@users.noreply.github.com>
2024-12-06 18:44:59 +0100

    Solutions for Day6-1 and Day6-2

---
+++ Day6/Day6-1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#define INPUT_PATH "C:\\Users\\franz\\CLionProjects\\AdventOfCode2024\\Day6\\input.txt"
+
+using namespace std;
+
+
+enum {
+    visited,
+    unvisited,
+    blocked
+};
+
+int main() {
+    int result = 1;
+
+    vector<vector<char>> lab;
+    ifstream InFile(INPUT_PATH);
+    int direction = 0b10;
+    // 1 0
+    // | | 0 x-axis, 1 y-axis
+    // | 0 positive, 1 negative
+    string currentLine;
+    pair<int, int> guard;
+    for (int row = 0; getline(InFile, currentLine); ++row) {
+        vector<char> tempRow;
+        for (int column = 0; column < currentLine.size(); ++column) {
+            switch (currentLine[column]) {
+                case '#':
+                    tempRow.push_back(blocked);
+                    break;
+                case '^':
+                    guard = make_pair(row, column);
+                    tempRow.push_back(visited);
+                    break;
+                default:
+                    tempRow.push_back(unvisited);
+            }
+        }
+        lab.push_back(tempRow);
+    }
+    int height = static_cast<int>(lab.size()) - 1;
+    int width = static_cast<int>(lab.at(0).size()) - 1;
+
+    while (guard.first < height && guard.first > 0 && guard.second < width && guard.second > 0) {
+        int nextX = guard.first + ((!(direction & 0b01)) * (0b01 - (direction & 0b10))); //Determine movement
+        int nextY = guard.second + ((direction & 0b01) * (0b01 - (direction & 0b10))); //Determine movement
+        if (lab[nextX][nextY] != blocked) {
+            guard.first = nextX;
+            guard.second = nextY;
+            result += lab[nextX][nextY]; //Add value of field
+            lab[nextX][nextY] = visited;
+        } else {
+            direction = (direction + 3) % 4; //Turn right
+        }
+    }
+    cout << result;
+    return 0;
+}
---
+++ Day6/Day6-2.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#define INPUT_PATH "C:\\Users\\franz\\CLionProjects\\AdventOfCode2024\\Day6\\input.txt"
+
+using namespace std;
+
+int main() {
+    int result = 0;
+
+    vector<vector<char>> labClear;
+    ifstream InFile(INPUT_PATH);
+
+    string currentLine;
+    pair<int, int> guardStart;
+    for (int row = 0; getline(InFile, currentLine); ++row) {
+        vector<char> tempRow;
+        for (int column = 0; column < currentLine.size(); ++column) {
+            switch (currentLine[column]) {
+                case '#':
+                    tempRow.push_back(-1);
+                    break;
+                case '^':
+                    guardStart = make_pair(row, column);
+                default:
+                    tempRow.push_back(0);
+            }
+        }
+        labClear.push_back(tempRow);
+    }
+    int height = static_cast<int>(labClear.size()) - 1;
+    int width = static_cast<int>(labClear.at(0).size()) - 1;
+
+    for (int boxRow = 0; boxRow <= height; ++boxRow) {
+        for (int boxColumn = 0; boxColumn <= width; ++boxColumn) {
+            if(labClear[boxRow][boxColumn] == -1) continue;
+            //RESET
+            pair<int, int> guard = guardStart;
+            vector<vector<char>> lab = labClear;
+            int direction = 0b10;
+            // 1 0
+            // | | 0 x-axis, 1 y-axis
+            // | 0 positive, 1 negative
+
+            //Place Box
+            lab[boxRow][boxColumn] = -1;
+
+            while (guard.first < height && guard.first > 0 && guard.second < width && guard.second > 0) {
+                int nextY = guard.first + ((!(direction & 0b01)) * (0b01 - (direction & 0b10))); //Determine movement
+                int nextX = guard.second + ((direction & 0b01) * (0b01 - (direction & 0b10))); //Determine movement
+                if(lab[nextY][nextX] > 3){
+                    //loop, visited same field at least 3 times
+                    ++result;
+                    break;
+                }else if (lab[nextY][nextX] == -1) {
+                    //obstacle
+                    direction = (direction + 3) % 4;
+                } else {
+                    //no obstacle
+                    guard.first = nextY;
+                    guard.second = nextX;
+                    lab[nextY][nextX] += 1;
+                }
+            }
+
+        }
+    }
+    cout << result;
+    return 0;
+}
