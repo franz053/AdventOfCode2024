09f04707a987b33d7112b94257067bc4119dcf96
franz053 <139059326+franz053@users.noreply.github.com>
2025-10-06 16:42:24 +0200

    Solution for Day12-1

--- Day12/Day12-1.cpp
+++ Day12/Day12-1.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 #define INPUT_PATH "..\\Day12\\input.txt"
 
-struct record {
-    long area = 0;
-    long perimeter = 0;
-};
+
+void traversePlot(const int row, const int column, std::vector<std::string> &farm, int& area, int& perimeter) {
+    char plant = farm[row][column];
+    farm[row][column] = '#'; //already visited marker
+    area++;
+    //Check around for perimeter
+    if (farm[row-1][column] != plant && farm[row-1][column] != '#') perimeter++;
+    if (farm[row+1][column] != plant && farm[row+1][column] != '#') perimeter++;
+    if (farm[row][column-1] != plant && farm[row][column-1] != '#') perimeter++;
+    if (farm[row][column+1] != plant && farm[row][column+1] != '#') perimeter++;
+
+    //Check for neighbors to continue
+    if (farm[row-1][column] == plant) traversePlot(row-1,column,farm,area,perimeter);
+    if (farm[row+1][column] == plant) traversePlot(row+1,column,farm,area,perimeter);
+    if (farm[row][column-1] == plant) traversePlot(row,column-1,farm,area,perimeter);
+    if (farm[row][column+1] == plant) traversePlot(row,column+1,farm,area,perimeter);
+
+    farm[row][column] = ' ';
+}
+
+//returns price of plot and deletes it from the map
+int settlePlot(int row, int column, std::vector<std::string> &farm) {
+    int area = 0, perimeter = 0;
+    traversePlot(row, column, farm, area, perimeter);
+    return area*perimeter;
+}
 
 int main() {
+    unsigned long price = 0;
+
     std::vector<std::string> farm;
 
     //Read in file
@@ -19,40 +42,22 @@ int main() {
     inputFile.open(INPUT_PATH);
     if (inputFile.is_open()) {
         std::string line;
-        while (std::getline(inputFile, line)) farm.push_back("  " + line + " "); //Padding for later
+        while (std::getline(inputFile, line)) farm.push_back(" " + line + " "); //Padding for later
         inputFile.close();
     } else {
         std::cerr << "Error opening file " << INPUT_PATH << std::endl;
     }
 
     //Add padding
-    farm.insert(farm.begin(), std::string(farm[0].length(), ' '));
-    farm.insert(farm.begin(), std::string(farm[0].length(), ' '));
+    farm.insert(farm.begin(),std::string(farm[0].length(),' '));
     farm.emplace_back(farm[0].length(), ' ');
 
-    //first = area, second = perimeter
-    std::unordered_map<char, record> records;
-
-    for (int row = 2; row < farm.size() - 1; ++row) {
-        for (int column = 2; column < farm[0].length() - 1; ++column) {
-            char element = farm[row][column];
-
-            //calc area
-            records[element].area += 1; //counts all areas
-
-            //calc perimeter
-            if (farm[row + 1][column] != element) records[element].perimeter += 1; //if element south is different
-            if (farm[row - 1][column] != element) records[element].perimeter += 1; //if element south is different
-            if (farm[row][column + 1] != element) records[element].perimeter += 1; //if element south is different
-            if (farm[row][column - 1] != element) records[element].perimeter += 1; //if element south is different
+    for (int row = 1; row < farm.size() - 1; ++row) {
+        for (int column = 1; column < farm[0].length() - 1; ++column) {
+            if (farm[row][column] != ' ') price += settlePlot(row, column, farm);
         }
     }
 
-    //calculate price
-    unsigned long price = 0;
-    for (auto [key, value]: records) {
-        price += value.area * value.perimeter;
-    }
     std::cout << price << std::endl;
     return 0;
 }
